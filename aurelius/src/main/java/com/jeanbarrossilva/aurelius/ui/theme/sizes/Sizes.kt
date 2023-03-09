8267d21f637bc9520d1ccc91826b8462c50226c3
Jean Silva <jeanbarrossilva@outlook.com>
2023-03-09 13:09:47 -0300

    change: internalize Sizes' contructor

--- aurelius/src/main/java/com/jeanbarrossilva/aurelius/ui/theme/sizes/Sizes.kt
+++ aurelius/src/main/java/com/jeanbarrossilva/aurelius/ui/theme/sizes/Sizes.kt
@@ -7,7 +7,7 @@ package com.jeanbarrossilva.aurelius.ui.theme.sizes
  * padding.
  * @param margin Reserved spaces for hierarchically greater components.
  **/
-data class Sizes(val spacing: Spacing, val margin: Margin) {
+data class Sizes internal constructor(val spacing: Spacing, val margin: Margin) {
     companion object {
         /** [Sizes] with unspecified values. **/
         internal val Unspecified = Sizes(Spacing.Unspecified, Margin.Unspecified)
