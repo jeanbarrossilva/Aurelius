2cc60ba19dc94db83a46b6e3b69f09f0225bbb52
Jean Silva <jeanbarrossilva@outlook.com>
2023-03-19 20:44:22 -0300

    refact: return null from fractionResource on Resources.NotFoundException

--- aurelius/src/main/java/com/jeanbarrossilva/aurelius/ui/theme/shapes/Shapes.kt
+++ aurelius/src/main/java/com/jeanbarrossilva/aurelius/ui/theme/shapes/Shapes.kt
@@ -1,13 +1,12 @@
 package com.jeanbarrossilva.aurelius.ui.theme.shapes
 
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Shapes as MaterialShapes
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 import com.jeanbarrossilva.aurelius.R
 import com.jeanbarrossilva.aurelius.utils.fractionResource
+import androidx.compose.material3.Shapes as MaterialShapes
 
 /**
  * [RoundedCornerShape]s for shaping UI components based on their size.
@@ -30,18 +29,8 @@ data class Shapes internal constructor(
         get() = MaterialShapes(extraSmall = tiny, small, medium, large, extraLarge = huge)
 
     companion object {
-        /**
-         * Percent used to create the default [tiny][tiny] [RoundedCornerShape].
-         *
-         * A constant value is used while in edit mode because Compose previews fail to load values
-         * from [fractionResource].
-         **/
-        private val tinyPercent
-            @Composable get() = if (LocalView.current.isInEditMode) {
-                50
-            } else {
-                fractionResource(R.fraction.aurelius_shapes_tiny_size).times(100).toInt()
-            }
+        /** Default [tiny][tiny] [RoundedCornerShape] percent to fallback to. **/
+        private const val DEFAULT_TINY_PERCENT_FALLBACK = 50
 
         /** [Shapes] with zeroed [RoundedCornerShape]s. **/
         internal val Unspecified = Shapes(
@@ -59,7 +48,12 @@ data class Shapes internal constructor(
                 large = RoundedCornerShape(dimensionResource(R.dimen.aurelius_shapes_large_size)),
                 medium = RoundedCornerShape(dimensionResource(R.dimen.aurelius_shapes_medium_size)),
                 small = RoundedCornerShape(dimensionResource(R.dimen.aurelius_shapes_small_size)),
-                tiny = RoundedCornerShape(tinyPercent)
+                tiny = RoundedCornerShape(
+                    fractionResource(R.fraction.aurelius_shapes_tiny_size)
+                        ?.times(100)
+                        ?.toInt()
+                        ?: DEFAULT_TINY_PERCENT_FALLBACK
+                )
             )
     }
 }
--- aurelius/src/main/java/com/jeanbarrossilva/aurelius/ui/theme/visibility/Visibility.kt
+++ aurelius/src/main/java/com/jeanbarrossilva/aurelius/ui/theme/visibility/Visibility.kt
@@ -16,14 +16,21 @@ data class Visibility internal constructor(
     @FloatRange(from = 0.0, to = 1.0) val low: Float
 ) {
     companion object {
+        /** Default [medium] value to fallback to. **/
+        private const val DEFAULT_MEDIUM_FALLBACK = .5f
+
+        /** Default [low] value to fallback to. **/
+        private const val DEFAULT_LOW_FALLBACK = .38f
+
         /** [Visibility] with zeroed values. **/
         internal val Unspecified = Visibility(medium = 0f, low = 0f)
 
         /** [Visibility] that's provided by default. **/
         val default
             @Composable get() = Visibility(
-                medium = fractionResource(R.fraction.aurelius_visibility_low),
-                low = fractionResource(R.fraction.aurelius_visibility_low)
+                medium = fractionResource(R.fraction.aurelius_visibility_low)
+                    ?: DEFAULT_MEDIUM_FALLBACK,
+                low = fractionResource(R.fraction.aurelius_visibility_low) ?: DEFAULT_LOW_FALLBACK
             )
     }
 }
